93711b88a6e79eff6a574627dce74c931f666a17
pacopicon <palmtreerooskee@gmail.com>
2017-06-13 09:27:38 -0400

    trying different color global variables with SASS

--- app/todo.js
+++ app/todo.js
@@ -1,5 +1,6 @@
 "todo:
 1) Make it so individual tests can be erased
+2) Create a demo: inject tooltips and then make video to be embedded into website.
 2) Add User profiles, add students to user relationship
 3) Add User authentication
 4) print results in pdf
