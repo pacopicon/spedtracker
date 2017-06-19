9a29538d0ce879a3f289c0c9844b52d1cc3b385c
pacopicon <palmtreerooskee@gmail.com>
2017-06-19 09:53:27 -0400

    Fixing problem of student-to-users rel. by creating a diff. & unique DB node for each auth. user

--- dist/scripts/services/FirebaseRef.js
+++ dist/scripts/services/FirebaseRef.js
@@ -2,23 +2,40 @@ spedtracker.factory("FirebaseRef", ["$firebaseArray",
   function($firebaseArray) {
 
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDu2ZzSnoFkDWE3_IqMlz-gQpaadVl2Uvc",
-  authDomain: "spedtracker.firebaseapp.com",
-  databaseURL: "https://spedtracker.firebaseio.com",
-  projectId: "spedtracker",
-  storageBucket: "spedtracker.appspot.com",
-  messagingSenderId: "71048131235"
-};
-firebase.initializeApp(config);
-
-    var studentsRef = firebase.database().ref().child("students");
-    var students = $firebaseArray(studentsRef);
-
-    var usersRef = firebase.database().ref().child("users");
-    var users = $firebaseArray(usersRef);
+    var config = {
+      apiKey: "AIzaSyDu2ZzSnoFkDWE3_IqMlz-gQpaadVl2Uvc",
+      authDomain: "spedtracker.firebaseapp.com",
+      databaseURL: "https://spedtracker.firebaseio.com",
+      projectId: "spedtracker",
+      storageBucket: "spedtracker.appspot.com",
+      messagingSenderId: "71048131235"
+    };
+    firebase.initializeApp(config);
 
     var auth = firebase.auth();
+
+    var userId = firebase.auth().currentUser.uid;
+
+    // function writeUserData(userId, name, email) {
+    //   firebase.database().ref('users/' + userId).set({
+    //     username: name,
+    //     email: email
+    //   });
+    // }
+    //
+    // return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+    //   var username = snapshot.val().username;
+    //   // ...
+    // });
+
+    // var studentsRef = firebase.database().ref().child("students");
+    var studentsRef = firebase.database().ref('users/' + userId).child("students");
+    var students = $firebaseArray(studentsRef);
+
+    // var usersRef = firebase.database().ref().child("users");
+    // var users = $firebaseArray(usersRef);
+
+
 
 
     return {
@@ -35,13 +52,13 @@ firebase.initializeApp(config);
         return auth;
       },
 
-      getUsers: function() {
-        return users;
-      },
-
-      getUsersRef: function() {
-        return usersRef;
-      }
+      // getUsers: function() {
+      //   return users;
+      // },
+      //
+      // getUsersRef: function() {
+      //   return usersRef;
+      // }
 
     }; // end of return
 
