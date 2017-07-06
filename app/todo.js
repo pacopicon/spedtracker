0e31de26700bae9fb0308fd2505793e999b48bec
Paco <palmtreerooskee@gmail.com>
2017-07-06 06:49:44 -0400

    optimized for 2nd deploy

--- app/todo.js
+++ app/todo.js
@@ -1,4 +1,7 @@
 "todo:
-5) Host Website and propagate it
-6) Create a demo: inject tooltips and then make video to be embedded into website.
-7) Put comments / ratings Feed so users and other potential stakeholders get a sense of app usefulness
+1) Host Website and propagate it
+2) Create a demo: inject tooltips and then make video to be embedded into website.
+3) Put comments / ratings Feed so users and other potential stakeholders get a sense of app usefulness
+4) Make site more responsive to mobile
+5) fix 'view data printout' and 'destroy data' buttonsAndBars
+6) 
